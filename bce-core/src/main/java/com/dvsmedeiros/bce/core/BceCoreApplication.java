bc0d8a0cfdc5cc2c4892e75797ce03da20607841
Davisson <dvisson.medeiros@gmail.com>
2017-05-25 22:46:39 -0300

    altera bcc-core de  @SpringBootApplication para @Configuration
    alteração realizada para que o contexto do framework possa ser
    utilizado em uma aplicação cliente

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/BceCoreApplication.java
+++ bce-core/src/main/java/com/dvsmedeiros/bce/core/BceCoreApplication.java
@@ -1,20 +1,13 @@
 package com.dvsmedeiros.bce.core;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages="com.dvsmedeiros")
-@EnableJpaRepositories(basePackages="com.dvsmedeiros")
-@EntityScan(basePackages="com.dvsmedeiros")
-@SpringBootApplication
+@ComponentScan("com.dvsmedeiros")
+@EnableJpaRepositories("com.dvsmedeiros")
+@EntityScan("com.dvsmedeiros")
+@Configuration
 public class BceCoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BceCoreApplication.class, args);
-	}
 }
