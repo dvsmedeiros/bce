b8b99ca952d6295e34c874d554daa460bf07dec8
Davisson <dvisson.medeiros@gmail.com>
2017-05-26 22:45:14 -0300

    corrige query do deleleByCode

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/repository/GenericSpecificRepository.java
+++ bce-core/src/main/java/com/dvsmedeiros/bce/core/repository/GenericSpecificRepository.java
@@ -2,6 +2,9 @@ package com.dvsmedeiros.bce.core.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 
 
@@ -12,5 +15,7 @@ public interface GenericSpecificRepository<T extends DomainSpecificEntity>
 
 	List<T> findByActive(Boolean active);
 
+	@Modifying
+    @Query("delete from DomainSpecificEntity d where d.code = ?1")
 	void deleteByCode(String code);
 }
