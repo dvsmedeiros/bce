4c8f26694afd00bd07dcb8097f1d2c39d7c9361b
Davisson <dvisson.medeiros@gmail.com>
2017-09-06 03:12:30 -0300

    add base controller com configuração de Mapping e CrossOrigin
    A configuração do mapeamento é realizada atrever da propertie:
    bce.app.context=<text> no application.properties

---
+++ bce-rest/src/main/java/com/dvsmedeiros/rest/rest/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.dvsmedeiros.rest.rest.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("${bce.app.context}")
+public class BaseController {
+	
+}
