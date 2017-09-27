c7a130cc07b45b60809b23dee2336fc79f6063fc
Davisson <dvisson.medeiros@gmail.com>
2017-09-27 09:09:13 -0300

    add componente de autenticação

---
+++ bce-auth/src/main/java/com/dvsmedeiros/auth/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.dvsmedeiros.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.dvsmedeiros.auth.filter.JWTAuthenticationFilter;
+import com.dvsmedeiros.auth.filter.JWTLoginFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable().authorizeRequests()
+			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			
+			// filtra requisições de login
+			.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+			
+			// filtra outras requisições para verificar a presença do JWT no header
+			.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+				.withUser("admin")
+				.password("admin")
+				.roles("ADMIN");
+	}
+}
---
+++ bce-auth/src/main/java/com/dvsmedeiros/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package com.dvsmedeiros.auth.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.dvsmedeiros.auth.token.TokenAuthenticationService;
+
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+
+		Authentication authentication = TokenAuthenticationService.getAuthentication((HttpServletRequest) request);
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		filterChain.doFilter(request, response);
+	}
+
+}
