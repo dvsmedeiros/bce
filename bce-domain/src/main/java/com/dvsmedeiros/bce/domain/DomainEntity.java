0298f7d76de4b6fda58a79816df2bfea99690be3
Davisson <dvisson.medeiros@gmail.com>
2017-11-11 00:04:40 -0200

    add hashCode e equals e altera tipo de geração do Id para SEQUENCE

--- bce-domain/src/main/java/com/dvsmedeiros/bce/domain/DomainEntity.java
+++ bce-domain/src/main/java/com/dvsmedeiros/bce/domain/DomainEntity.java
@@ -11,16 +11,19 @@ import javax.persistence.InheritanceType;
 import javax.persistence.PrePersist;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @JsonInclude(Include.NON_NULL)
+//@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
 public abstract class DomainEntity extends AbstractDomainEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE) 
 	private long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
 	private Calendar insertionDate;
@@ -44,6 +47,28 @@ public abstract class DomainEntity extends AbstractDomainEntity {
 	@PrePersist
 	public void prePersist() {
 		this.insertionDate = Calendar.getInstance();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DomainEntity other = (DomainEntity) obj;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 }
