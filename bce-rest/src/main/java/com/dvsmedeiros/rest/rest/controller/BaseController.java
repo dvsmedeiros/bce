d221a798d9a25cf6a8d0fcfc0eef600f7dc24261
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-09-17 14:40:05 -0300

    corrige propertie com a versão do bce e remove dependência com spring security
    a remoção da dependência com o spring security tem o objetivo de transferir a reponsabilidade de segurança para a aplicação que consumirá o framework

--- bce-rest/src/main/java/com/dvsmedeiros/rest/rest/controller/BaseController.java
+++ bce-rest/src/main/java/com/dvsmedeiros/rest/rest/controller/BaseController.java
@@ -1,18 +1,11 @@
 package com.dvsmedeiros.rest.rest.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dvsmedeiros.bce.domain.ApplicationEntity;
 
+@RequestMapping("/")
 @CrossOrigin(origins = "*")
-@RequestMapping("${bce.app.context}") 
 public class BaseController extends ApplicationEntity {
-	
-	public Authentication getLoggedAuthentication() {
-		return SecurityContextHolder.getContext().getAuthentication();
-	}
-		
 }
