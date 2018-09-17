0e3847bf1d7b9bc694b85b507890be37105d7ad9
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-09-17 14:42:08 -0300

    remove configuration dao
    find by code está disponível no genericSpecificDAO

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/dao/IConfigurationDAO.java
+++
@@ -1,8 +0,0 @@
-package com.dvsmedeiros.bce.core.dao;
-
-import com.dvsmedeiros.bce.domain.Configuration;
-
-public interface IConfigurationDAO extends IDAO<Configuration>{
-	
-	Configuration findByCode(String code);
-}
