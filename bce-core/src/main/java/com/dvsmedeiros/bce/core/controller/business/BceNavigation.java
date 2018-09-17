3431fcbf8f9ffc8d0025bcc3a2d4529bfc021749
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-09-17 14:41:29 -0300

    remove scheduler

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/controller/business/BceNavigation.java
+++ bce-core/src/main/java/com/dvsmedeiros/bce/core/controller/business/BceNavigation.java
@@ -3,12 +3,10 @@ package com.dvsmedeiros.bce.core.controller.business;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import com.dvsmedeiros.bce.core.controller.business.impl.DeleteByCodeStrategy;
 import com.dvsmedeiros.bce.core.controller.impl.Navigation;
 import com.dvsmedeiros.bce.core.controller.impl.NavigationBuilder;
-import com.dvsmedeiros.bce.core.controller.impl.RestartScheduler;
 import com.dvsmedeiros.bce.domain.DomainSpecificEntity;
 
 @Configuration
@@ -16,24 +14,11 @@ public class BceNavigation {
 
 	@Autowired
 	private DeleteByCodeStrategy deleteByCode;
-	@Autowired
-	private RestartScheduler restartScheduler;	
 
 	@Bean(name = "DELETE_BY_CODE")
 	public Navigation<DomainSpecificEntity> deleteByCode() {
-
 		return new NavigationBuilder<DomainSpecificEntity>()
 				.next(deleteByCode)
 				.build();
-
-	}
-	
-	@Bean(name = "RESTART_SCHEDULER")
-	public Navigation<com.dvsmedeiros.bce.domain.Configuration> updateConfiguration() {
-
-		return new NavigationBuilder<com.dvsmedeiros.bce.domain.Configuration>()
-				.next(restartScheduler)
-				.build();
-
 	}
 }
--- bce-rest/src/main/java/com/dvsmedeiros/rest/rest/controller/ConfigurationController.java
+++ bce-rest/src/main/java/com/dvsmedeiros/rest/rest/controller/ConfigurationController.java
@@ -1,47 +1,26 @@
 package com.dvsmedeiros.rest.rest.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.dvsmedeiros.bce.core.controller.IFacade;
-import com.dvsmedeiros.bce.core.controller.INavigator;
-import com.dvsmedeiros.bce.core.controller.impl.BusinessCaseBuilder;
 import com.dvsmedeiros.bce.domain.Configuration;
 
+@SuppressWarnings("rawtypes")
 @Controller
 @RequestMapping("configuration")
-@SuppressWarnings("rawtypes")
 public class ConfigurationController extends DomainSpecificEntityController<Configuration> {
-	
+
 	public ConfigurationController() {
 		super(Configuration.class);
 	}
 
-
-	@Autowired
-	@Qualifier("applicationFacade")
-	private IFacade<Configuration> appFacade;
-	
-	@Autowired
-	@Qualifier("navigator")
-	private INavigator navigator;
-		
 	@Override
 	@CacheEvict(value = "cacheConfigurations", allEntries = true)
 	public ResponseEntity updateEntity(@RequestBody Configuration entity) {
-		
-		super.updateEntity(entity);
-		
-		navigator.run(entity, new BusinessCaseBuilder<>().withName("RESTART_SCHEDULER").build());
-		return new ResponseEntity<Configuration>(HttpStatus.OK);
-		
+		return super.updateEntity(entity);
 	}
-	
+
 }
