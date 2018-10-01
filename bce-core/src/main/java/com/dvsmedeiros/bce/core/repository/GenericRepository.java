80cccea178a0ea9637a060db1ee223e8796b9822
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-10-01 20:25:48 -0300

    add métodos findByCode, findByActive, activate, inactivate

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/repository/GenericRepository.java
+++ bce-core/src/main/java/com/dvsmedeiros/bce/core/repository/GenericRepository.java
@@ -1,9 +1,24 @@
 package com.dvsmedeiros.bce.core.repository;
 
+import java.util.stream.Stream;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dvsmedeiros.bce.domain.DomainEntity;
 
 public interface GenericRepository<T extends DomainEntity> extends CrudRepository<T, Long> {
+
+	@Query("select t from #{#entityName} t where t.code = ?1")
+	T findByCode(String code);
+
+	@Query("select t from #{#entityName} t where t.active = ?1")
+	Stream<T> findByActive(Boolean active);
+
+	@Query("update #{#entityName} t set t.active = true where t.code = ?1")
+	T activate(String code);
+
+	@Query("update #{#entityName} t set t.active = false where t.code = ?1")
+	T inactivate(String code);
 
 }
