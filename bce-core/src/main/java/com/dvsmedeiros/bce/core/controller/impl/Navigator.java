4bee92643b6fd748183f2c2947dbf4edbb319a89
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-10-02 11:38:25 -0300

    add log com os nomes das navegações

--- bce-core/src/main/java/com/dvsmedeiros/bce/core/controller/impl/Navigator.java
+++ bce-core/src/main/java/com/dvsmedeiros/bce/core/controller/impl/Navigator.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,13 +19,22 @@ import com.dvsmedeiros.bce.domain.IEntity;
 @Component
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Navigator<E extends IEntity> implements INavigator {
-
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private NavigatorContext context;
 
 	@Autowired
 	private Map<String, Navigation<E>> listNavigations = new HashMap<String, Navigation<E>>();
-
+	
+	@PostConstruct
+	public void init() {
+		logger.info("==============================NAVIGATIONS==============================");
+		listNavigations.keySet().forEach(logger::info);
+		logger.info("=======================================================================");
+	}
+	
 	@Override
 	public void run(IEntity aEntity, INavigationCase aCase) {
 
